4d2ea741d338bd48ae4eee120b526fdddd136b95
Трохимец Константин <katrohimets@mail.ru>
2024-06-03 19:23:09 +0300

    feat: env.example and config for pm2

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Trohimets/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./{.env,.env.deploy} ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v16.20.2/bin/ && npm i && npm run build',
+    },
+  },
+};
